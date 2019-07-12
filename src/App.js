e19726f6932dffbee9ce6d4cccfe81e66e06f4f8
CristianManosalva <manosalvacristian98@gmail.com>
2019-07-12 12:34:02 -0500

    Introduciendo array de tareas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navegation';
+import tasks from './tasks.json';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation title="Holi Papa" />
-        <Navigation title="Holi Mama" />
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
