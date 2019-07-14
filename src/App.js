d254966bda93201ac181b1ace63742a46b482b79
CristianManosalva <manosalvacristian98@gmail.com>
2019-07-13 19:51:25 -0500

    Se agrego el compenente formulario y se esta tratando de conectar con el componente padre

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navegation';
-import tasks from './tasks.json';
-
+import FormTasks from './components/FormTasks';
+import { tasks } from './tasks.json';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+       tasks
+    }
+  }
   render() {
+
+    const tasksComponent = this.state.tasks.map((task, i) => {
+      return(
+        <div className="col-md-4">
+            <div className="card mt-4">
+              <div className="card-header">
+                <h3>{task.title}</h3>
+                <span className="badge badge-pill badge-danger ml-2">
+                  {task.priority}
+                </span>
+              </div>
+              <div className="card-body">
+                <p>{task.description}</p>
+              </div>
+              <div className="card-footer">
+                <p><mark>{task.responsible}</mark></p>
+              </div>
+            </div>
+        </div>
+      )
+    })
+
     return (
       <div className="App">
-        <Navigation title="Holi Papa" />
+        <Navigation title="Program Tasks" numberTasks={this.state.tasks.length}/>
+        <FormTasks />
+        <div className="container">
+          <div className="row mt-4">
+            {tasksComponent}
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
